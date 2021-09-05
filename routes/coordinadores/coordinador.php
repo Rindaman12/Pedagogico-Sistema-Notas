7badbca2cbd75c17f32a1cbcfd0194bc90514479
Luis Peña <luispena1098@gmail.com>
2021-09-05 14:10:37 -0400

    Fixed Coordinador en la app

--- routes/coordinadores/coordinador.php
+++ routes/coordinadores/coordinador.php
@@ -21,36 +21,9 @@ use Illuminate\Support\Facades\Session;
 Route::group(['middleware' => 'auth'], function () {
     Route::get('coordinador/inicio', [CoordinadorController::class, 'index']);
 
-    Route::get('/coordinador/constancia/incepcion', function () {
-        $user = Auth::user();
+    Route::get('/coordinador/constancia/incepcion', [CoordinadorController::class, 'index']);
 
-        if ($user->tipo == 'coordinador') {
-            return view('coordinador.constancia_incepcion');
-        } else {
-            Session::flush();
-            return redirect()->route('login');
-        }
-    })->name('coordinador.constancia_incepcion');
+    Route::get('/coordinador/listados', [CoordinadorController::class, 'index']);
 
-    Route::get('/coordinador/listados', function () {
-        $user = Auth::user();
-
-        if ($user->tipo == 'coordinador') {
-            return view('coordinador.listados');
-        } else {
-            Session::flush();
-            return redirect()->route('login');
-        }
-    })->name('coordinador.listados');
-
-    Route::get('/coordinador/record', function () {
-        $user = Auth::user();
-
-        if ($user->tipo == 'coordinador') {
-            return view('coordinador.record');
-        } else {
-            Session::flush();
-            return redirect()->route('login');
-        }
-    })->name('coordinador.record');
+    Route::get('/coordinador/record', [CoordinadorController::class, 'index']);
 });
